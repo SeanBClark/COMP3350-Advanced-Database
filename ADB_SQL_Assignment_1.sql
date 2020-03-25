cb56359a312566d10c3c1200fb1fa451dbaed4f4
AaronMoss1 <AaronMoss1@users.noreply.github.com>
2020-03-25 22:03:08 +1100

    Update

--- ADB_SQL_Assignment_1.sql
+++ ADB_SQL_Assignment_1.sql
@@ -1,117 +1,35 @@
--- create database ABD_SQL_Assignment_1
+--if not exists(select * from sys.databases where name = 'UniDB')
+--    create database UniDB
+--GO
+--USE UniDB
+--GO
 
--- need to give it a better name?
-create table organisation_units (
+DROP TABLE Contact_Number
+DROP TABLE Address
+DROP TABLE Name
 
-    ou_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-
+CREATE TABLE Name (
+Name_ID INT PRIMARY KEY IDENTITY(1, 1),
+First_Name VARCHAR(40) NOT NULL,
+Middle_Name VARCHAR(40),
+Last_Name VARCHAR(40) NOT NULL
 )
 
--- need to give it a better name?
-create table sub_organisation_units (
-
-    sou_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-    ou_PK char(15) unique not NUll,
-
-    FOREIGN KEY (ou_PK) REFERENCES organisation_units(ou_PK) ON UPDATE NO ACTION ON DELETE NO ACTION,
-
+CREATE TABLE Address (
+Address_ID INT PRIMARY KEY IDENTITY(1,1),
+Street_No INT NOT NULL,
+Street VARCHAR(70) NOT NULL,
+City VARCHAR(50) NOT NULL,
+Post_Code VARCHAR(6) NOT NULL,
+State VARCHAR(10) NOT NULL,
+Country VARCHAR(50) NOT NULL
 )
 
-create table staff (
-
-    staff_PK char(15) PRIMARY key not NUll,
-    staff_ID char(15) unique not NUll,
-    ou_PK char(15) unique not NUll,
-    sou_PK_PK char(15) unique not NUll,
-
-    FOREIGN KEY (ou_PK) REFERENCES sub_organisation_units(ou_PK) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    FOREIGN KEY (sou_PK) REFERENCES organisation_units(sou_PK) ON UPDATE NO ACTION ON DELETE NO ACTION,
-
-)
-
-create table course_cord (
-
-    cc_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-    staff_pk char(15) unique not NUll,
-
-    FOREIGN KEY (staff_pk) REFERENCES staff(staff_pk) ON UPDATE NO ACTION ON DELETE NO ACTION,
-
-)
-
-create table program_convin (
-
-    pc_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-    staff_pk char(15) unique not NUll,
-
-    FOREIGN KEY (staff_pk) REFERENCES staff(staff_pk) ON UPDATE NO ACTION ON DELETE NO ACTION,
-
-)
-
-create table acad_program (
-
-    acad_pro_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-
-)
-
-create table major_minor (
-
-    major_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-    acad_pro_PK char(15) unique not NUll,
-
-    FOREIGN KEY (acad_pro_PK) REFERENCES acad_program(acad_pro_PK) ON UPDATE NO ACTION ON DELETE NO ACTION,
-
-)
-
-create table locations (
-
-    loc_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-
-)
-
-
-create table faclilties (
-
-    fac_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-    loc_PK char(15) unique not NUll,
-
-    FOREIGN KEY (loc_PK) REFERENCES locations(loc_PK) ON UPDATE NO ACTION ON DELETE NO ACTION,
-
-)
-
-create table sems_period (
-
-    sp_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-
-)
-
-
-create table course (
-
-    cor_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-    sp_PK char(15) unique not NUll,
-    fac_PK char(15) unique not NUll,
-
-    FOREIGN KEY (sp_PK) REFERENCES sems_period(sp_PK) ON UPDATE NO ACTION ON DELETE NO ACTION,
-    FOREIGN KEY (fac_PK) REFERENCES faclilties(fac_PK) ON UPDATE NO ACTION ON DELETE NO ACTION,
-
-)
-
-create table student (
-
-    stu_PK char(15) PRIMARY key not NUll,
-    _ID char(15) unique not NUll,
-    stu_no char(15) unique not NUll,
-
-    -- FKs to courses they are doing or some shit
-
+CREATE TABLE Contact_Number (
+Contact_ID INT PRIMARY KEY IDENTITY(1,1),
+Home_Number VARCHAR(30),
+Work_Number VARCHAR(30),
+Mobile_Number VARCHAR(20),
+Additional_Number_1 VARCHAR(30),
+Additional_Number_2 VARCHAR(30)
 )

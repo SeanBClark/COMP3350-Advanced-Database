0886f417126bd78129bab6d2bf61338cb3dde5fc
AaronMoss1 <AaronMoss1@users.noreply.github.com>
2020-03-26 00:47:13 +1100

    Update 3

--- ADB_SQL_Assignment_1.sql
+++ ADB_SQL_Assignment_1.sql
@@ -3,6 +3,15 @@
 --GO
 --USE UniDB
 --GO
+
+DROP TABLE CourseProgramAssign
+DROP TABLE GroupCourseAssign
+DROP TABLE PreReqCourseGroup
+DROP TABLE Course
+DROP TABLE Student_Enrolments
+DROP TABLE Program_Convenor_Assign
+DROP TABLE Program
+DROP TABLE Semester_Trimester
 DROP TABLE Organisational_Unit_Register
 DROP TABLE SubOrganisation_Unit
 DROP TABLE Organisation_Unit
@@ -11,6 +20,7 @@ DROP TABLE Course_Coordinator
 DROP TABLE Academic_Staff
 DROP TABLE Admin_Staff
 DROP TABLE Staff
+DROP TABLE Student
 DROP TABLE Contact_Number
 DROP TABLE Address
 DROP TABLE Name
@@ -39,6 +49,16 @@ Work_Number VARCHAR(30),
 Mobile_Number VARCHAR(20),
 Additional_Number_1 VARCHAR(30),
 Additional_Number_2 VARCHAR(30)
+)
+
+CREATE TABLE Student (
+Student_ID INT PRIMARY KEY IDENTITY(1,1),
+Name_ID INT UNIQUE, --can uniquely identify each student member by name
+Address_ID INT, --May live in the same house, does not have to be unique
+Contact_ID INT UNIQUE, --Student must have contact number unique to them
+FOREIGN KEY (Name_ID) REFERENCES Name(Name_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (Address_ID) REFERENCES Address(Address_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (Contact_ID) REFERENCES Contact_Number(Contact_ID) ON UPDATE CASCADE ON DELETE NO ACTION
 )
 
 CREATE TABLE Staff (
@@ -93,9 +113,78 @@ Staff_ID INT,
 Organisation_ID INT,
 StartDate DATE NOT NULL,
 EndDate DATE,
-Role_Played VARCHAR(75),
+Role_Played VARCHAR(75) NOT NULL,
 FOREIGN KEY (Staff_ID) REFERENCES Staff(Staff_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
 FOREIGN KEY (Organisation_ID) REFERENCES Organisation_Unit(Organisation_ID) ON UPDATE CASCADE ON DELETE NO ACTION
 )
 
+CREATE TABLE Semester_Trimester (
+SemTriSem_ID INT PRIMARY KEY IDENTITY(1,1),
+Name VARCHAR(30) NOT NULL,
+Year INT NOT NULL
+)
 
+CREATE TABLE Program (
+Program_ID INT PRIMARY KEY IDENTITY,
+Organisation_ID INT,
+Program_Code CHAR(8) NOT NULL, --Must be 8 characters long and be formed as such: ABCD1234
+Prog_Name VARCHAR(80) UNIQUE NOT NULL,
+Total_Credits INT NOT NULL,
+Prog_Level VARCHAR(30) NOT NULL, --Contains Certificate, Bachelor, masters, phd
+Cert_Achieved VARCHAR(20) NOT NULL,
+FOREIGN KEY (Organisation_ID) REFERENCES Organisation_Unit(Organisation_ID) ON UPDATE CASCADE ON DELETE NO ACTION
+)
+
+CREATE TABLE Program_Convenor_Assign ( --Table used for assigning a program convenor to a program, will match a convenor code, with a program code
+Program_ID INT NOT NULL,
+Staff_ID INT NOT NULL,
+PRIMARY KEY(Program_ID, Staff_ID),
+FOREIGN KEY (Program_ID) REFERENCES Program(Program_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (Staff_ID) REFERENCES Program_Convenor(Staff_ID) ON UPDATE CASCADE ON DELETE NO ACTION
+)
+
+CREATE TABLE Student_Enrolments (
+ProgramEnrol_ID INT PRIMARY KEY IDENTITY(1,1),
+Student_ID INT,
+Program_ID INT,
+SemTriSem_ID INT,
+StartDate DATE NOT NULL,
+EndDate DATE,
+Status VARCHAR(30) NOT NULL DEFAULT 'In Progress',
+FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (Program_ID) REFERENCES Program(Program_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (SemTriSem_ID) REFERENCES Semester_Trimester(SemTriSem_ID) ON UPDATE CASCADE ON DELETE NO ACTION
+)
+
+CREATE TABLE Course ( --Just holds general courses
+Course_ID INT PRIMARY KEY IDENTITY(1,1),
+Name VARCHAR(30) UNIQUE NOT NULL,
+Credits INT NOT NULL,
+Description TEXT NOT NULL
+)
+
+CREATE TABLE PreReqCourseGroup ( -- Holds a number, which will be an ID of a group that holds mutliple courses that are pre requisites for another course
+Group_ID INT PRIMARY KEY
+)
+
+CREATE TABLE GroupCourseAssign ( --takes a groupID and a courseID, a group has many courses a part of it, this table allows us to reference what courses a groupID Holds
+--for example: Group 10 has course 1, 2, 3.... and group 11 has courses 1,3,6,7 <<< group 10 and 11 store pre requisite courses for another course
+Course_ID INT,
+Group_ID INT,
+PRIMARY KEY(Course_ID, Group_ID),
+FOREIGN KEY (Course_ID) REFERENCES Course(Course_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (Group_ID) REFERENCES PreReqCourseGroup(Group_ID) ON UPDATE CASCADE ON DELETE NO ACTION
+)
+
+CREATE TABLE CourseProgramAssign ( --this keeps track of a course assigned to a program, group PK references the pre reqs for the course
+CourseProgID INT PRIMARY KEY IDENTITY(1,1),
+Course_ID INT,
+Program_ID INT,
+Group_ID INT,
+StartDate DATE NOT NULL,
+EndDate DATE,
+Course_Type VARCHAR(30), --can be core, directed or compulsory for example
+FOREIGN KEY (Course_ID) REFERENCES Course(Course_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (Program_ID) REFERENCES Program(Program_ID) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY (Group_ID) REFERENCES PreReqCourseGroup(Group_ID) ON UPDATE CASCADE ON DELETE NO ACTION
+)

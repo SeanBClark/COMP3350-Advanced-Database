ee08c29b25a30cd938906dbc2cea1f2b85167d8e
AaronMoss1 <AaronMoss1@users.noreply.github.com>
2020-03-28 12:14:57 +1100

    removed comments

--- TEST-TESTTRIGGER.sql
+++ TEST-TESTTRIGGER.sql
@@ -40,32 +40,6 @@ IF @Result > 0
 
 END
 
---DECLARE @ex INT = 1; --test purposes
-----BEGIN
---IF @ex = 1
---	BEGIN
---		PRINT(@CourseOfferID);
---		PRINT(@StudentID);
---	END
---ELSE
---	BEGIN
---		RAISERROR('no insert', 1, 1);
---		ROLLBACK TRANSACTION;
---	END
-----END
-
 DEALLOCATE courseCollect
 
 END
-
---DECLARE @ex INT = 1; --test purposes
---IF @ex = 1 --this is a test to see printed values
---	BEGIN --must BEGIN every if and else statement and then END it
---		PRINT(@CourseOfferID);
---		PRINT(@StudentID);
---	END
---ELSE
---	BEGIN
---		RAISERROR('no insert', 1, 1);
---		ROLLBACK TRANSACTION;
---	END

32de38ce6ade30abd48ae290e8e24386e904f195
SeanBClark <58195450+SeanBClark@users.noreply.github.com>
2020-05-02 11:37:05 +1000

    Local Host issue

--- Assignment_2/QueryTest.sql
+++ Assignment_2/QueryTest.sql
@@ -16,15 +16,15 @@ SELECT * FROM Fact.Sale
 --GROUP BY month(Fact.Sale.[Invoice Date Key]), year(Fact.Sale.[Invoice Date Key])
 --ORDER BY year(Fact.Sale.[Invoice Date Key]), month(Fact.Sale.[Invoice Date Key])
 
-SELECT  year(Fact.Sale.[Invoice Date Key]) as Year, datename(month, Fact.Sale.[Invoice Date Key]) as Month, sum(Fact.Sale.[Total Excluding Tax]) as [Monthly Sales Amount]
-FROM Fact.Sale INNER JOIN Dimension.City
-ON Fact.Sale.[City Key] = Dimension.City.[City Key] and Dimension.City.[State Province] = 'Florida'
-GROUP BY month(Fact.Sale.[Invoice Date Key]), datename(month, Fact.Sale.[Invoice Date Key]), year(Fact.Sale.[Invoice Date Key])
-ORDER BY year(Fact.Sale.[Invoice Date Key]), month(Fact.Sale.[Invoice Date Key])
+--SELECT  year(Fact.Sale.[Invoice Date Key]) as Year, datename(month, Fact.Sale.[Invoice Date Key]) as Month, sum(Fact.Sale.[Total Excluding Tax]) as [Monthly Sales Amount]
+--FROM Fact.Sale INNER JOIN Dimension.City
+--ON Fact.Sale.[City Key] = Dimension.City.[City Key] and Dimension.City.[State Province] = 'Florida'
+--GROUP BY month(Fact.Sale.[Invoice Date Key]), datename(month, Fact.Sale.[Invoice Date Key]), year(Fact.Sale.[Invoice Date Key])
+--ORDER BY year(Fact.Sale.[Invoice Date Key]), month(Fact.Sale.[Invoice Date Key])
 
 
-SELECT year(Fact.Sale.[Invoice Date Key]) as Year, sum(Fact.Sale.[Total Excluding Tax]) as [Yearly Sales]
-FROM Fact.Sale INNER JOIN Dimension.City
-ON Fact.Sale.[City Key] = Dimension.City.[City Key] and Dimension.City.[State Province] = 'Florida'
-GROUP BY year(Fact.Sale.[Invoice Date Key])
-ORDER BY year(Fact.Sale.[Invoice Date Key])
+--SELECT year(Fact.Sale.[Invoice Date Key]) as Year, sum(Fact.Sale.[Total Excluding Tax]) as [Yearly Sales]
+--FROM Fact.Sale INNER JOIN Dimension.City
+--ON Fact.Sale.[City Key] = Dimension.City.[City Key] and Dimension.City.[State Province] = 'Florida'
+--GROUP BY year(Fact.Sale.[Invoice Date Key])
+--ORDER BY year(Fact.Sale.[Invoice Date Key])

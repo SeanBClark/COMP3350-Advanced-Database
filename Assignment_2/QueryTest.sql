72ac0b367ccd1215f763eb0b084165bd2cc1bad9
SeanBClark <58195450+SeanBClark@users.noreply.github.com>
2020-05-02 17:53:12 +1000

    Excel

--- Assignment_2/QueryTest.sql
+++ Assignment_2/QueryTest.sql
@@ -4,6 +4,14 @@ SELECT * FROM Dimension.Customer
 
 SELECT * FROM Fact.Sale
 
+select * from Dimension.Date;
+
+select * from fact.Purchase
+
+select * from Dimension.[Transaction Type]
+
+select * from Dimension.Employee
+
 --SELECT sum(Fact.Sale.[Total Excluding Tax]) as Sales, Fact.Sale.[City Key], Dimension.City.[City Key], year(Fact.Sale.[Invoice Date Key]) as Year, Dimension.City.[State Province]
 --FROM Fact.Sale INNER JOIN Dimension.City
 --ON Fact.Sale.[City Key] = Dimension.City.[City Key] and Dimension.City.[State Province] = 'Florida'
@@ -28,3 +36,23 @@ SELECT * FROM Fact.Sale
 --ON Fact.Sale.[City Key] = Dimension.City.[City Key] and Dimension.City.[State Province] = 'Florida'
 --GROUP BY year(Fact.Sale.[Invoice Date Key])
 --ORDER BY year(Fact.Sale.[Invoice Date Key])
+
+select top 10 count(Fact.Sale.Quantity) as SaleCount, Fact.Sale.Description from Fact.Sale group by Fact.Sale.Description order by count(Fact.Sale.Quantity) desc;
+
+select top 10 count(Fact.Sale.Quantity) as SaleCount, Fact.Sale.Description from Fact.Sale group by Fact.Sale.Description order by count(Fact.Sale.Quantity) asc;
+
+select top 10 count(Fact.Sale.[Customer Key]) as [Top Sales], Dimension.Customer.[Customer Key], Dimension.Customer.Customer from Fact.Sale 
+	inner join Dimension.Customer on Fact.Sale.[Customer Key] = Dimension.Customer.[Customer Key] 
+	where Dimension.Customer.Customer != 'Unknown' 
+	group by Dimension.Customer.[Customer Key], Dimension.Customer.Customer
+	order by count(Fact.Sale.[Customer Key]) desc;
+
+select top 10 count(Fact.Sale.[Customer Key]) as [Least Sales], Dimension.Customer.[Customer Key], Dimension.Customer.Customer from Fact.Sale 
+	inner join Dimension.Customer on Fact.Sale.[Customer Key] = Dimension.Customer.[Customer Key] 
+	where Dimension.Customer.Customer != 'Unknown' 
+	group by Dimension.Customer.[Customer Key], Dimension.Customer.Customer
+	order by count(Fact.Sale.[Customer Key]) asc;
+
+select top 10 count(Fact.Sale.Profit) as SaleCount, Fact.Sale.Description from Fact.Sale group by Fact.Sale.Description order by count(Fact.Sale.Profit) desc;
+
+
